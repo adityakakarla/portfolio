1900a8a5ebc6fe7e37cb055374e25322bf7a3ba6
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-01-20 23:54:17 -0800

    updated

--- global.js
+++ global.js
@@ -16,7 +16,7 @@ let pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
-    { url: 'portfolio/', title: 'Resume' },
+    { url: 'resume/', title: 'Resume' },
     { url: 'https://github.com/adityakakarla', title: 'Github' }
 ]
 
