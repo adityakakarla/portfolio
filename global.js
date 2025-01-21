b360e7d633b1345fb1a85171eb63cc6902fd2aa3
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-01-20 23:35:16 -0800

    trying again?

--- global.js
+++ global.js
@@ -28,7 +28,7 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home')
 for (let p of pages){
     let url = p.url
     if (!ARE_WE_HOME && !url.startsWith('http')){
-        url = '/portfolio/' + url
+        url = '../' + url
     }
     let title = p.title
     let a = document.createElement('a')
